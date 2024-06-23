d21a031a2976da909c51ed2a067c63f1092b7046
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-06-23 18:46:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        s = s.toLowerCase();
+        boolean check = true;
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : s.toCharArray())
+        {
+            if(c ==' ')
+            {
+                check = true;
+            }
+            else if(check)
+            {
+                c = Character.toUpperCase(c);
+                check = false;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
