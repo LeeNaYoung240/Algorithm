c232301aa3a4ada976f073f221f4ae09850176b4
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-07-07 20:48:51 +0900

    [level 3] Title: 단속카메라, Time: 7.01 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,19 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
-        // 진출 지점을 기준으로 오름차순 정렬
-        Arrays.sort(routes, (a,b) -> Integer.compare(a[1],b[1]));
-        
-        int min = Integer.MIN_VALUE;
-        for(int[] route : routes)
-        {
-            if(min<route[0])
-            {
-                min = route[1];
-                answer++;
-            }
-        }
         return answer;
     }
 }
