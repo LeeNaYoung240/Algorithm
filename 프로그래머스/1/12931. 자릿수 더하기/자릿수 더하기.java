e8ca26603d4a7f1cfa0cc19bfef8630d2af42b6f
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-09-23 18:01:41 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int sum = 0;
-        while(n>0)
-        {
-            sum+=n%10;
-            int mok = n/10;
-            n = mok;
-        }
+        int answer = 0;
         
-        return sum;
+        while(n!=0)
+        {
+            answer+=(n%10);
+            n/=10;
+        }
+        return answer;
     }
 }
