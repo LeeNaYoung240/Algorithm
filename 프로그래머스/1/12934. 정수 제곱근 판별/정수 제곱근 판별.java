740df773aa666566ac3f590aad5fd3236b9f023a
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-09-24 15:12:51 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.17 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,13 +1,18 @@
+import java.util.*;
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        double x = Math.sqrt(n);
-        if(x%1==0)
+        double sqrt = Math.sqrt(n); // 제곱근 구하기
+        long x = (long)sqrt; // 소수점 버리고 타입 변환
+        if(x*x == n)
         {
-            answer = ((long)Math.pow(x+1,2));
+            answer = (x+1)*(x+1);
         }
         else
+        {
             answer = -1;
+        }
+        
         return answer;
     }
 }
