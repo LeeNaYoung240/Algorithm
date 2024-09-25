585ed87f1220e9a84ec723f0b3e85dea0a3d92fa
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-09-25 19:56:32 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.53 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -2,24 +2,22 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int idx = 0;
-        int m1=0, m2 =0;
-        int result = 0;
-        
+        int answer_Idx1 = 0, answer_Idx2 = 0;
+        int n1=0, m1 =0;
+      
         for(int i=1;i<=Math.min(n,m);i++)
         {
             if(n%i==0 && m%i==0)
             {
-                idx = i; // 최대공약수
+                answer_Idx1 = i; 
             }
-          
         }
-        m1 = n / idx;
-        m2 = m / idx;
-        result = idx * m1 * m2;
+        n1 = n / answer_Idx1;
+        m1 = m / answer_Idx1;
+        answer_Idx2 = answer_Idx1 * n1 * m1;
     
-        answer[0] = idx;
-        answer[1] = result; 
+        answer[0] = answer_Idx1;
+        answer[1] = answer_Idx2; 
     
         return answer;
     }
