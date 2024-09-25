ab2c15eba879d3696186b90604d6e0ef458df71c
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-09-25 18:54:30 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean solution(String s) {
-       if(s.length() == 4 || s.length()==6)
+       if(s.length() != 4 && s.length()!=6)
        {
-           try{
-               int x = Integer.parseInt(s);
-               return true;
-           }
-           catch(NumberFormatException e)
-           {
-               return false;
-           }
+           return false;
        }
-        else{
-            return false;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)<'0' || s.charAt(i)>'9')
+            {
+                return false;
+            }
         }
+       return true;
     }
 }
