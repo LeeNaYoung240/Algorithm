add297a242ea08430b64d3c1894927d5deaeadfd
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-09-25 16:33:03 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 21.60 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,14 +1,27 @@
+import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        int len = s.length();
-        if(len%2==0)
+        char[] ch = s.toCharArray(); // 문자열을 문자 배열로
+        int cnt = 0;
+        for(int i=0;i<ch.length;i++)
         {
-            answer = s.substring(len/2-1,len/2+1);
+            cnt++;
+        }
+        int idx = 0;
+        int idx2 = 0;
+        if(cnt%2==1)
+        {
+            idx = ch.length/2; // 인덱스 번호 저장
+            answer += ch[idx];
         }
         else{
-            answer = s.substring(len/2,len/2+1);
+            idx = ch.length/2;
+            idx2 = ch.length/2-1;
+            answer += ch[idx2];
+            answer += ch[idx];
         }
+        
         return answer;
     }
 }
