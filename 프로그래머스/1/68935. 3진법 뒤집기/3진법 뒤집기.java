ed2ffe56679123398bde3575d0d47ba6836e8e28
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-09-26 11:47:07 +0900

    [level 1] Title: 3진법 뒤집기, Time: 15.37 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,14 +2,13 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String s = Integer.toString(n,3); // 3진법으로 변환
+        String s = Integer.toString(n, 3); // 10진수 -> 3진법
         String str = "";
         for(int i=s.length()-1;i>=0;i--)
         {
             str += s.charAt(i);
         }
-        answer = Integer.parseInt(str, 3);
-        
+        answer = Integer.parseInt(str, 3); // str을 3진법으로 해석해서 10진법으로 변환
         return answer;
     }
 }
