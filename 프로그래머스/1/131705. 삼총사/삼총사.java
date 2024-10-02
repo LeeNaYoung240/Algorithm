69121cb7c84551bae0b722db05f355f31e7a2196
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-10-02 14:33:06 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,7 +1,5 @@
-import java.util.*;
 class Solution {
     public int solution(int[] number) {
-        int answer = 0;
         int cnt = 0;
         for(int i=0;i<number.length-2;i++)
         {
@@ -9,7 +7,7 @@ class Solution {
             {
                 for(int k=j+1;k<number.length;k++)
                 {
-                     if(number[i]+number[j]+number[k]==0)
+                    if(number[i]+number[j]+number[k]==0)
                     {
                         cnt++;
                     }
