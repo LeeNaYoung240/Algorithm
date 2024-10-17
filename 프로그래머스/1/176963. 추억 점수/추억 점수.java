5cd81b3560a226baf9fb908c8b9409e3941ad16e
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-10-17 20:10:14 +0900

    [level 1] Title: 추억 점수, Time: 2.26 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -3,19 +3,24 @@ class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
         
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0;i<name.length;i++)
+        {
+            map.put(name[i],yearning[i]);
+        }
+        
         for(int i=0;i<photo.length;i++)
         {
             for(int j=0;j<photo[i].length;j++)
             {
-                for(int k=0;k<name.length;k++)
+                if(map.containsKey(photo[i][j]))
                 {
-                    if(name[k].equals(photo[i][j]))
-                    {
-                        answer[i] += yearning[k];
-                    }
+                    answer[i] += map.get(photo[i][j]);
                 }
             }
         }
+        
         return answer;
     }
 }
