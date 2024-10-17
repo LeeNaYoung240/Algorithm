b8c2f9cc34c04e7a7df6a41f1fc9bf1f3f2b7d24
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-10-17 19:01:59 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.00 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,17 +2,22 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> li = new ArrayList<>();
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); //우선순위 큐 선언
         
         for(int i=0;i<score.length;i++)
         {
-            li.add(score[i]);
-            if(li.size()>k)
+            pq.add(score[i]); //pq에 요소 저장
+            
+            if(pq.size()>k) //pq의 크기가 k보다 크면
             {
-                li.remove(Collections.min(li));
+                pq.poll(); //가장 작은 값 제거
             }
-            answer[i] = Collections.min(li);
+            
+            answer[i] = pq.peek(); // 가장 작은 값 조회
         }
+        
+        
         return answer;
     }
 }
