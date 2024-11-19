083876836eb43b1cca6bee3b238700b4d9babf97
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-11-19 18:13:24 +0900

    [level 1] Title: 체육복, Time: 0.15 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
+        
+        for(int l : lost)
+        {
+            lostSet.add(l);
+        }
+        for(int r : reserve)
+        {
+            if(lostSet.contains(r))
+            {
+                lostSet.remove(r);
+            }
+            else{
+                reserveSet.add(r);
+            }
+        }
+        
+        for(int l : lostSet)
+        {
+            if(reserveSet.contains(l-1))
+            {
+                reserveSet.remove(l-1);
+            }
+            else if(reserveSet.contains(l+1))
+            {
+                reserveSet.remove(l+1);
+            }
+            else{
+                answer--; 
+            }
+        }
+        
+        return answer;
+    }
+}
