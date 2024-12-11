923e09ecfe1b8a88597c3daf0484eec46e299d12
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-12-11 14:10:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.21 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,11 +2,12 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        
         Map<String, Integer> map = new HashMap<>();
         
         for(String s : participant)
         {
-            map.put(s, map.getOrDefault(s,0)+1);
+            map.put(s, map.getOrDefault(s, 0)+1);
         }
         
         for(String s : completion)
