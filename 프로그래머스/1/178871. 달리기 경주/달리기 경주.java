06186c7a25d7e482b7b445c005fbd7114c99b522
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-12-12 16:00:00 +0900

    [level 1] Title: 달리기 경주, Time: 463.74 ms, Memory: 361 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -5,21 +5,20 @@ class Solution {
         
         for(int i=0;i<players.length;i++)
         {
-            map.put(players[i], i); // 이름, 순위
+            map.put(players[i],i);
+        }
+        for(String s : callings)
+        {
+            int grade = map.get(s); // 호출된 플레이어 등수
+            
+            String forwardCalling = players[grade-1]; // 호출한 플레이어 직전의 사람
+            players[grade-1] = s; // 호출한 사람 해당 자리로 업데이트
+            map.put(s, grade-1); //map에 순위 한칸 앞으로 이동
+            
+            players[grade] = forwardCalling; // 앞에 있던 사람 등수 뒤로 이동
+            map.put(forwardCalling, grade);
         }
         
-        for(String calling : callings)
-        {
-            int grade = map.get(calling); // 호출된 이름의 현재 순위
-            
-            String forwardCalling = players[grade-1]; // 해당 순위 직전의 players 이름을 가져옴
-            
-            players[grade-1] = calling; // 호출한 플레이어를 앞으로 이동
-            map.put(calling, grade -1); // map에서 순위 한칸 앞으로 이동
-            
-            players[grade] = forwardCalling; // 앞에 있던 플레이어 뒤로 이동
-            map.put(forwardCalling, grade); //map에서 순위 업데이트
-        }
         return players;
     }
 }
