0049df0c6622bdf8977330e57842fc6c338a19c3
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2024-12-16 10:18:09 +0900

    [level 1] Title: 폰켓몬, Time: 6.43 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,20 +2,20 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int num = nums.length/2;
-        HashSet<Integer> set = new HashSet<>();
+        int num = nums.length / 2;
+        Set<Integer> set = new HashSet<>();
         
         for(int i=0;i<nums.length;i++)
         {
             set.add(nums[i]);
         }
         
-        if(num < set.size())
+        if(set.size() < num)
         {
-            answer = num;
+            answer = set.size();
         }
         else{
-            answer = set.size();
+            answer = num;
         }
         
         return answer;
