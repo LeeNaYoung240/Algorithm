2f0df41f68a2cc3c0cd314a204604c7fe3ce471a
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2025-01-02 17:07:38 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 36.38 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,40 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int size = truck_weights.length; //다리 위를 건너야 할 트럭의 수
+        int current = 0; // 다리 위에 올라간 트럭 무게
+        int idx = 0;
+        
         Queue<Integer> q = new LinkedList<>();
         
-        for(int i=0;i<bridge_length-1;i++)
-        {
-            q.add(0);
-        }
-        
-        int current_w = truck_weights[0];
-        q.add(current_w);
-        
-        int answer = 1;
-        int idx = 1;
-        
-        while(!q.isEmpty())
-        {
-            answer++;
-            
-            int removed = (int) q.poll();
-            current_w -= removed;
-            
-            if(idx < truck_weights.length)
+        do{
+            if(q.size() == bridge_length)
             {
-                if(current_w + truck_weights[idx] <= weight)
-                {
-                    current_w += truck_weights[idx];
-                    q.add(truck_weights[idx]);
-                    idx++;
-                }
-                else{
-                    q.add(0);
-                }
+                current -= q.poll(); // 다리 위에 트럭이 꽉 차면 다리를 건넌 트럭을 큐에서 제거하고 그 무게를 빼줌
             }
+            if(idx<size && current + truck_weights[idx] <= weight) // 다리위에 트럭이 올라갈 수 있다면
+            {
+                q.add(truck_weights[idx]); // 큐에 해당 인덱스 번호의 무게를 추가
+                current += truck_weights[idx++]; // 무게에 해당 무게를 추가
+            }
+            else
+                q.add(0); // 트럭이 올라갈 수 없다면 0 추가
             
-        }
+            answer++; // 시간 증가
+            
+        } while(current != 0); // 모든 트럭이 다리를 건널 때까지 반복
+        
         
         return answer;
     }
