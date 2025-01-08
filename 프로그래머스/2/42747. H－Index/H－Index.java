ed2c0fde7e5feb35f117ac984ee6b4bb1bc0b0e0
나영 <107848521+LeeNaYoung240@users.noreply.github.com>
2025-01-08 21:41:42 +0900

    [level 2] Title: H-Index, Time: 1.21 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,11 +2,10 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int len = citations.length;
         
         Arrays.sort(citations);
         
-        for(int i=0;i<len;i++)
+        for(int i=0;i<citations.length;i++)
         {
             int h = citations.length-i;
             
